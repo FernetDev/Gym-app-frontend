a00c2cb32fa5267d3fa3b1d336bc69ba66adf8d8
balta <baltaasanchez@gmail.com>
2024-10-31 23:18:51 -0300

    Add Pages

--- Gym-app-front/src/app/Components/dashboard/dashboard.component.ts
+++ Gym-app-front/src/app/Components/dashboard/dashboard.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import { RouterLinkActive, RouterOutlet } from '@angular/router';
+import {MatListModule} from '@angular/material/list';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [MatSidenavModule, RouterOutlet, MatListModule,RouterLinkActive, RouterLink, MatIconModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
