82932bfaf3863e6628a36fd8333e84f4d299e3e7
balta <baltaasanchez@gmail.com>
2024-11-04 15:43:47 -0300

    Style payments component

--- Gym-app-front/src/app/Pages/payments/payments.component.ts
+++ Gym-app-front/src/app/Pages/payments/payments.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatFormFieldModule, MatLabel } from "@angular/material/form-field";
+import { MatInput, MatInputModule } from '@angular/material/input';
+
 
 @Component({
   selector: 'app-payments',
   standalone: true,
-  imports: [],
+  imports: [ MatFormFieldModule,MatInputModule,MatLabel,MatInput],
   templateUrl: './payments.component.html',
   styleUrl: './payments.component.css'
 })
