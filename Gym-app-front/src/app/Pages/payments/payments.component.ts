9300e7c45ade1769b6f7c6dc6516159e816352de
balta <baltaasanchez@gmail.com>
2024-11-06 01:08:53 -0300

    Add formbuilder to payment component

--- Gym-app-front/src/app/Pages/payments/payments.component.ts
+++ Gym-app-front/src/app/Pages/payments/payments.component.ts
@@ -1,14 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-payments',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule],
   templateUrl: './payments.component.html',
   styleUrls: ['./payments.component.css']
 })
-export class PaymentsComponent { }
+export class PaymentsComponent implements OnInit{ 
+myForm! : FormGroup;
+
+constructor(private fb: FormBuilder){}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required]],
+      price: ['', [Validators.required]],
+      date: ['', [Validators.required]],
+    }, );
+  }
+
+  onSubmit(){
+    if(this.myForm.valid){
+      console.log("Form submited", this.myForm.value);
+    }else{
+      console.log("invalid", this.myForm.value);
+    }
+  }
+
+
+
+}
