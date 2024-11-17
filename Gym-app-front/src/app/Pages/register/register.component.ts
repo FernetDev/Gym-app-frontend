ab8d6f833c93389685cfbe984c14a5f242594ec1
maidanaen <maidanaenzosebastian@gmail.com>
2024-11-17 19:14:41 -0300

    componente registro

--- Gym-app-front/src/app/Pages/register/register.component.ts
+++ Gym-app-front/src/app/Pages/register/register.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatNativeDateModule } from '@angular/material/core';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports:  [ReactiveFormsModule,
+     MatFormFieldModule,
+     MatInputModule,
+     MatDatepickerModule,
+     MatNativeDateModule,
+     MatButtonModule,
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
-
+  
 }
