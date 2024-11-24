32cf0def54ba087ffaa3aa17030fb7dcc5fd3168
balta <baltaasanchez@gmail.com>
2024-11-24 02:50:46 -0300

    Edit payments logic and styles

--- Gym-app-front/src/app/Pages/payments/payments.component.ts
+++ Gym-app-front/src/app/Pages/payments/payments.component.ts
@@ -2,26 +2,33 @@ import { Component, OnInit } from '@angular/core';
 import { MatDatepickerModule } from '@angular/material/datepicker';
 import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule, provideNativeDateAdapter } from '@angular/material/core';
+import { MatNativeDateModule, MatOption, provideNativeDateAdapter } from '@angular/material/core';
 import { FormBuilder,FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { NgIf } from '@angular/common';
+import { NgFor, NgIf } from '@angular/common';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
 
 @Component({
   selector: 'app-payments',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule, NgIf],
+  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule, MatNativeDateModule, ReactiveFormsModule, NgIf,MatAutocompleteModule, NgFor,MatOption],
   templateUrl: './payments.component.html',
   styleUrls: ['./payments.component.css']
 })
 export class PaymentsComponent implements OnInit{ 
+  options = [
+    { label: 'Básico', value: 1 },
+    { label: 'Personalizado', value: 2 },
+    { label: 'Powerlifter', value: 3 }
+  ];
 myForm! : FormGroup;
 
 constructor(private fb: FormBuilder){}
 
+
   ngOnInit(): void {
     this.myForm = this.fb.group({
       name: ['', [Validators.required]],
-      price: ['', [Validators.required,Validators.pattern('^[0-9]*$')]],
+      fechaIngreso: ['', [Validators.required,]],
       date: ['', [Validators.required]],
     }, );
   }
