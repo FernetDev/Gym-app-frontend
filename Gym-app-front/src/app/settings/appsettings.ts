28a38bf553d51d357b075b74405fec852313340c
balta <baltaasanchez@gmail.com>
2024-11-30 02:14:51 -0300

    update

--- Gym-app-front/src/app/settings/appsettings.ts
+++ Gym-app-front/src/app/settings/appsettings.ts
@@ -1,3 +1,3 @@
 export const appsettings = {
-    apiUrl: "https://ac19-2800-23a8-2080-2ed-452-51f0-2e0e-c38e.ngrok-free.app/api/"
+    apiUrl: "https://593d-2800-23a8-2080-2ed-452-51f0-2e0e-c38e.ngrok-free.app/api/"
 }
