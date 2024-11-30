20e4d3ea79653f245d9ab464c83971290be35090
balta <baltaasanchez@gmail.com>
2024-11-30 01:51:32 -0300

    Revancha con ngrok

--- Gym-app-front/src/app/settings/appsettings.ts
+++ Gym-app-front/src/app/settings/appsettings.ts
@@ -1,3 +1,3 @@
 export const appsettings = {
-    apiUrl: "https://1429-2800-23a8-2080-2ed-452-51f0-2e0e-c38e.ngrok-free.app/api/"
+    apiUrl: "https://ac19-2800-23a8-2080-2ed-452-51f0-2e0e-c38e.ngrok-free.app/api/"
 }
